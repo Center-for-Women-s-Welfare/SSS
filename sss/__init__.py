0a24e5caa53ef52f0163425dbd4e3322ade05e33
Bryna Hazelton <bryna.hazelton@gmail.com>
2022-07-29 12:04:07 -0700

    comment out internal imports for now
    We'll add them back once we fix the database setup.

--- sss/__init__.py
+++ sss/__init__.py
@@ -1,7 +1,7 @@
 from setuptools_scm import get_version
 
-from .sss import SSS, HealthCare, Miscellaneous, ARPA
-from .preprocess import std_col_names
+# from .sss import SSS, HealthCare, Miscellaneous, ARPA
+# from .preprocess import std_col_names
 
 __version__ = get_version()
 
