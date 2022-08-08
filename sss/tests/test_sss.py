c96da50622d14061439c8e8138b7267f87ce63db
Aziza Mirsaidova <amm2dv@mtmail.mtsu.edu>
2022-08-08 12:16:13 -0700

    Added test for read_file

--- sss/tests/test_sss.py
+++ sss/tests/test_sss.py
@@ -1,11 +1,26 @@
+import os
+
 from sss import sss_table
 from sss.data import DATA_PATH
 import sss
 
+def test_read_file(setup_and_teardown_package):
+    """ Test reading file """
+    df, file = sss_table.read_file(os.path.join(DATA_PATH, 'AR2022_SSS_Full.xlsx'))
 
-def test_create_database():
-    """  Test creating a database """
-    sss_table.data_folder_to_database(DATA_PATH)
+    expected_cols = ["family_type", "analysis_type"]
+
+    for col in expected_cols:
+        assert col in df.columns
+
+    assert len(df.columns) == 27
+    assert len(df) == 4
+
+    #session = setup_and_teardown_package.sessionmaker()
+
+# def test_create_database():
+#     """  Test creating a database """
+#     sss_table.data_folder_to_database(DATA_PATH)
 
 # def test_read_file():
 #     sss_table.read_file(file)
