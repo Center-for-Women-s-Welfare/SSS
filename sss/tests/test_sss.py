3ce181dace7fb8472bb2286bb533c0cec5287291
Aziza Mirsaidova <amm2dv@mtmail.mtsu.edu>
2022-08-09 11:36:05 -0700

    Additions to tests based on comments

--- sss/tests/test_sss.py
+++ sss/tests/test_sss.py
@@ -1,12 +1,11 @@
 import os
-
 import pytest
-
 from sss import sss_table
 from sss.data import DATA_PATH
 import sss
 from sss.base import AutomappedDB
 from sss.tests import test_db_url
+from sss import SSS
 
 def test_read_file(setup_and_teardown_package):
     """ Test reading file """
@@ -28,19 +27,26 @@ def test_check_extra_columns(setup_and_teardown_package):
     df, file = sss_table.read_file(os.path.join(DATA_PATH, 'AR2022_SSS_Full.xlsx'))
     df, miscellaneous, health_care, arpa = sss_table.check_extra_columns(df)
 
-    assert 'miscellaneous_is_secondary' and 'health_care_is_secondary' and 'analysis_is_secondary' in df.columns
+    columns_to_check = ['miscellaneous_is_secondary', 'health_care_is_secondary', 'analysis_is_secondary']
+
+    for col in columns_to_check:
+        assert col in df.columns
 
 def test_check_extra_columns_error(setup_and_teardown_package):
+    """ Test occurency of the error in checking the etxra column """
     with pytest.raises(ValueError, match="df should be a pandas dataframe."):
-        df, miscellaneous, health_care, arpa = sss_table.check_extra_columns((os.path.join(DATA_PATH)), 'AR2022_SSS_Full.xlsx')
+        df, miscellaneous, health_care, arpa = sss_table.check_extra_columns(os.path.join(DATA_PATH, 'AR2022_SSS_Full.xlsx'))
 
 def test_data_folder_to_database(setup_and_teardown_package):
-    db = AutomappedDB(test_db_url)
+    """ Test data folder to database """
+    db = setup_and_teardown_package
     session = db.sessionmaker()
-    sss_table.data_folder_to_database(os.path.join(DATA_PATH, db_url=test_db_url))
-    result = session.query('SSS').filter('SSS'.State == 'CA').all()
+    sss_table.data_folder_to_database(os.path.join(DATA_PATH), db_url=test_db_url)
+    result = session.query(SSS).filter(SSS.state == 'FL').all()
     assert len(result) == 4
     session.close()
+
+
 # def test_create_database():
 #     """  Test creating a database """
 #     sss_table.data_folder_to_database(DATA_PATH)
