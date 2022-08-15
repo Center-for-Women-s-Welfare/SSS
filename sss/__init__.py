e21a0b86b82d0974e8c7a3cab913680ac67eb65b
Bryna Hazelton <bryna.hazelton@gmail.com>
2022-08-15 16:04:59 -0700

    Fix the way the version is derived to work everywhere (#18)
    also update to suggested usage in pyproject.toml rather than setup.py

--- sss/__init__.py
+++ sss/__init__.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from pkg_resources import DistributionNotFound, get_distribution
 from setuptools_scm import get_version
 
 from .city import City
@@ -7,5 +10,16 @@ from .puma import PUMA
 from .report import Report
 from .preprocess import std_col_names
 
-__version__ = get_version()
+try:  # pragma: nocover
+    # get accurate version for developer installs
+    version_str = get_version(Path(__file__).parent.parent)
 
+    __version__ = version_str
+
+except (LookupError, ImportError):
+    try:
+        # Set the version automatically from the package details.
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:  # pragma: nocover
+        # package is not installed
+        pass
