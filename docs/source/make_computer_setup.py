24e1268da6a98afbf2a61e85fd2ceb22fa0a17bc
Priyana Patel <priyanap@uw.edu>
2022-08-18 15:46:52 -0700

    Adding first pass at sphinx docs

---
+++ docs/source/make_computer_setup.py
@@ -0,0 +1,48 @@
+# -*- mode: python; coding: utf-8 -*-
+
+"""
+Format the readme.md file into the sphinx index.rst file.
+"""
+import os
+import inspect
+
+import pypandoc
+import time
+
+
+def write_computer_setup_rst(input_file=None, write_file=None):
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+    out = (
+        ".. SSS documentation compuuter setup file, created by\n"
+        f"   make_computer_setup.py on {time_str}\n\n"
+    )
+
+    if input_file is None:
+        main_path = os.path.dirname(
+            os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+        )
+        input_file = os.path.join(main_path, "docs", "computer_setup.md")
+
+    input_text = pypandoc.convert_file(input_file, "rst")
+
+    # convert relative links in readme to explicit links
+    # readme_text = readme_text.replace(
+    #     "<docs/",
+    #     "<https://github.com/RadioAstronomySoftwareGroup/pyradiosky/tree/main/docs/",
+    # )
+
+    # readme_text = readme_text.replace(
+    #     "<.github/",
+    #     "<https://github.com/RadioAstronomySoftwareGroup/pyradiosky/tree/main/.github/",
+    # )
+
+    out += input_text
+
+    out.replace("\u2018", "'").replace("\u2019", "'").replace("\xa0", " ")
+
+    if write_file is None:
+        write_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+        write_file = os.path.join(write_path, "computer_setup.rst")
+    F = open(write_file, "w")
+    F.write(out)
+    print("wrote " + write_file)
