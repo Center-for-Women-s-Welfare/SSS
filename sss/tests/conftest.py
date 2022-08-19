8dce19693284cb1341d7235666453c3c87ef0845
Aziza Mirsaidova <amm2dv@mtmail.mtsu.edu>
2022-08-19 15:39:34 -0700

    Changed tmp test path

--- sss/tests/conftest.py
+++ sss/tests/conftest.py
@@ -4,12 +4,12 @@ import os
 import pytest
 
 from sss import base, sss_table
-from sss.tests import test_db_file
 from sss.data import DATA_PATH
 
 @pytest.fixture(autouse=True, scope="session")
-def setup_and_teardown_package():
+def setup_and_teardown_package(tmp_path_factory):
     
+    test_db_file = os.path.join(tmp_path_factory.mktemp('test'),'test_sss.sqlite')
     sss_declare = base.DeclarativeDB(db_file=test_db_file)
    
     # then we call the create table function 
