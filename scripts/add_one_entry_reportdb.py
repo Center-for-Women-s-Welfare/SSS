9d15b5a18b15128480537974b15eac966ad7cfdb
chengren <cnzjbob@gmail.com>
2022-08-24 12:11:31 -0700

    add script for geoid and report
    add script for geoid and report

---
+++ scripts/add_one_entry_reportdb.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Script for creating the tables
+"""
+
+import argparse
+from sss.report import add_one_entry_reportdb
+from sss.base import default_db_file
+from datetime import datetime, date
+
+# creating parser object 
+parser = argparse.ArgumentParser(description = "Creating the report database")
+
+# defining arguements for the parser object
+parser.add_argument("year", type = int,
+                    help = "Enter year of the record")
+
+parser.add_argument("state", type = str,
+                    help = "Enter state name")
+
+parser.add_argument("analysis_type", type = str,
+                    help = "Enter analysis type")
+
+parser.add_argument("cpi_month", type = str,
+                    help = "Enter month e.g. May")
+
+parser.add_argument("cpi_year", type = int,
+                    help = "Enter year of CPI report")
+
+parser.add_argument("update_date", type = date,
+                    help = "update date, the format is date(2021,6,22)")
+
+parser.add_argument("update_person", type = str,
+                    help = "who update the report")
+
+parser.add_argument("-d","--db_file", type = str,
+                    help = "Enter the database file that the data should be put in",
+                    default=default_db_file)
+
+args = parser.parse_args()
+
+# call the function with arguments
+add_one_entry_reportdb( args.year, 
+                        args.state,
+                        args.analysis_type,
+                        args.cpi_month,
+                        args.cpi_year,
+                        args.update_date,
+                        args.update_person,
+                        db_file=args.db_file)
