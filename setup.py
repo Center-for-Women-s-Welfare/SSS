183b6ac697e463489682a57ca25f7a824e59a34d
Bryna Hazelton <bryna.hazelton@gmail.com>
2023-04-21 11:22:30 -0700

    Add minimum version testing

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ opts = dict(
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires= [
-        "numpy",
+        "numpy>=1.21",
         "openpyxl>=3.1.0,!=3.1.1",
-        "pandas>=1.5",
-        "pyxlsb",
-        "setuptools_scm",
-        "sqlalchemy"
+        "pandas>=1.5.0",
+        "pyxlsb>=1.0.8",
+        "setuptools_scm>=7.0.3",
+        "sqlalchemy>=1.4.16"
     ],
     extras_require={
         "doc": ["sphinx", "pypandoc"],
