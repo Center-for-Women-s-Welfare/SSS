71c2c12e4de91ef3099b32e28db49b22496231a7
emenninga <eric.menninga@gmail.com>
2024-05-18 11:44:57 -0700

    Add the docstring required by pre-commit. Also some other minor formatting changes.

--- ID_tabletool/SSSnippet.py
+++ ID_tabletool/SSSnippet.py
@@ -1,20 +1,34 @@
+"""
+Read lines from an Excel file and copy the proper cells into an InDesign snippet.
+
+Primarily the help clas Snippet4ID is used to iterate through the Excel file.
+It will build an InDesign snippet file (XML format) that will represent
+a sequence of tables -- one for each county.
+There is a tight correspondence between this code and
+the format of the Excel tables and the InDesign snippets.
+"""
+
 # library for reading/writing xml files
 import lxml
+
 # library for regular expressions (complex find)
 import re
 
 NUM_COLUMNS_READ = 250  # columns of data (family types)
 
 
-# copy the numeric value into the cells in the snippet
-# rowdata = the Excel row with all the data
-# prevcell = the table cell in the snippet to the left of our target
-# columns = the list of column indices to insert
 def copy_numbers(rowdata, prevcell, columns: list, keep_cents: bool):
-    # for each column index (e.g. 'a', 'ai', 'ap', 'at')
+    """
+    Copy the numeric values from Excel into the cells in the XML snippet.
+
+    :param rowdata the Excel row with all the data
+    :param prevcell the table cell in the XML tree to the left of our target
+    :param columns: the list of column indices to insert
+    :param keep_cents: False = round to integers, else use 2 digits after decimal
+    """
     for colidx in columns:
         colvalue = rowdata[colidx]  # value in the Excel file in the right column
-        if type(colvalue) == str:  # if it's a string, make it a number
+        if type(colvalue) is str:  # if it's a string, make it a number
             colvalue = float(colvalue)
         if colvalue < 0.0:
             colvalue = "(${:,.0f})".format(-colvalue)
@@ -22,14 +36,23 @@ def copy_numbers(rowdata, prevcell, columns: list, keep_cents: bool):
             colvalue = "${:,.2f}".format(colvalue)
         else:
             colvalue = "${:,.0f}".format(colvalue)
-        prevcell = prevcell.getnext()  # move to the correct cell & replace content
-        prevcell[0][0][0].text = colvalue  # 1st paragraph - 2nd character range - Content
+        # move to the correct cell & replace content
+        prevcell = prevcell.getnext()
+        # 1st paragraph - 2nd character range - Content
+        prevcell[0][0][0].text = colvalue
 
 
 def sum_tax_amounts(rowdata: list, columns: list, amount_dict):
+    """
+    For the cells that are being calculated (taxes), track the sums.
+
+    :param rowdata the Excel row with all the data
+    :param columns the list of column indices to sum up
+    :param amount_dict store the sums here - key=column index, value=sum
+    """
     for colidx in columns:
         colvalue = rowdata[colidx]  # value in the Excel file in the right column
-        if type(colvalue) == str:  # if it's a string, make it a number
+        if type(colvalue) is str:  # if it's a string, make it a number
             colvalue = float(colvalue)
         if colidx in amount_dict:
             amount_dict[colidx] += colvalue
@@ -37,14 +60,19 @@ def sum_tax_amounts(rowdata: list, columns: list, amount_dict):
             amount_dict[colidx] = colvalue
 
 
-# complicated code to find the cells in the snippet XML to replace
-# rowhead = the row title from the Excel file (e.g. 'Housing')
-# root = the root of the XML tree representing one InDesign table snippet
-# find the corresponding row in the snippet that matches
 def row_label_match(rowhead: str, root):
+    """Return the XML element that matches the row description in the Excel.
+
+    For example, the Excel row starts with "Housing", so we look for "Housing"
+    in the XML content. There are minor differences between the Excel and the ID rows
+    :param rowhead the row title from the Excel file (e.g. 'Housing')
+    :param root the root of the XML tree representing one InDesign table snippet
+    :return the corresponding row in the snippet that matches
+    """
     xpath = "Cell//Content[.='" + rowhead + "']"
     cell = root.xpath(xpath)
-    if len(cell) == 0 and rowhead.find("\n") >= 0:  # newline in row header, just match first line
+    # newline in row header, just match first line
+    if len(cell) == 0 and rowhead.find("\n") >= 0:
         rowhead = rowhead.split("\n")[0]
         xpath = "Cell//Content[.='" + rowhead + "']"
         cell = root.xpath(xpath)
@@ -56,24 +84,27 @@ def row_label_match(rowhead: str, root):
     return cell
 
 
-# this class does the work of replacing content in the InDesign snippet tables
-# to reflect the data pulled from the Excel file
 class Snippet4ID:
-    # initialize the object
+    """
+    Primary function for building InDesign table snippets from Excel.
+
+    This functions kind of as an iterator, where append_table would be
+    called repeatedly appending each table to the same file.
+    """
+
     def __init__(self, idfilename: str):
-        # the path to use when generating the InDesign file
+        """Initialize the snippet with the ID snippet filename to create."""
         self.id_filename = idfilename
-        # open a new file at the location
         self.id_file = open(self.id_filename, "w", encoding="utf-8")
         # copy the "Pretable snippet" into the InDesign file (just copies, no parsing)
-        with open('PretableSnippet.xml', encoding="utf-8") as prefixfile:
+        with open("PretableSnippet.xml", encoding="utf-8") as prefixfile:
             prefix_data = prefixfile.read()
             self.id_file.write(prefix_data)
         # close the InDesign file (causes it to write to disk)
         # we open and close it repeatedly
         self.id_file.close()
         # read & parse the table snippet
-        self.snippet1_xml = lxml.etree.parse('TableSnippet.xml')
+        self.snippet1_xml = lxml.etree.parse("TableSnippet.xml")
         # get the root of the XML tree representation for the table
         self.snippet1_root = self.snippet1_xml.getroot()
         # start with empty columns (filled out later)
@@ -84,39 +115,59 @@ class Snippet4ID:
         # the row_iterator walks through the Excel file (row by row)
         self.row_iterator = None
         # list of columns summed as tax credits
-        self.tax_credits = ["Earned Income Tax Credit (-)", "Child Care Tax Credit (-)", "Child Tax Credit (-)"]
+        self.tax_credits = [
+            "Earned Income Tax Credit (-)",
+            "Child Care Tax Credit (-)",
+            "Child Tax Credit (-)",
+        ]
 
-    # table snippet has all IDs in the form u777777______ with some suffix,
-    # we change them to u 777777x+n _____ (increment the number part)
-    # where x is "1" for table 1 and "2" for table 2
-    def new_self_id(self, oldID: str, which: str) -> str:
-        if oldID.startswith(self.defaultTableID):
-            return self.nextTableID + which + oldID[7:]
-        return oldID
+    def _new_self_id(self, old_id: str) -> str:
+        """
+        Generate a new id for one element in the XML tree.
 
-    # create new IDs for the table items. Probably not used ?? but we make them unique anyway
-    def renumber(self):
+        :param old_id: the original id string in the snippet
+        :return: str representing the new unique id
+        """
+        if old_id.startswith(self.defaultTableID):
+            return self.nextTableID + old_id[7:]
+        return old_id
+
+    def _renumber(self):
+        """
+        Create new IDs for the table items in the snippet template.
+
+        Probably not used ?? but we make them unique anyway.
+        """
         self.nextTableID = "u" + str(int(self.nextTableID[1:]) + 1)
         for child in self.snippet1_root:
             if "Self" in child.attrib:
-                newid = self.new_self_id(child.attrib["Self"], "1")
-                child.attrib["Self"] = newid
+                new_id = self._new_self_id(child.attrib["Self"])
+                child.attrib["Self"] = new_id
 
-    # county / state / date table header -- only in table 1
-    # the Excel file has something like:
-    # 'Table 1_x000D_\nThe Self-Sufficiency Standard_x000D_\nfor_x000D_\nAllegany County, MD, 2023'
-    # we break this string into the 4 sections, pulling out relevant fields
-    def format_head(self, head_cell: str):
+    def _format_head(self, head_cell: str):
+        """
+        Copy the header data into the table snippet.
+
+        :param str head_cell: the contents of the Excel header cell
+        """
+        # The Excel file has something like --
+        #  'Table 1_x000D_\nThe Self-Sufficiency Standard_
+        #           x000D_\nfor_x000D_\nAllegany County, MD, 2023>
+        # We break this string into the 4 sections, copying the relevant fields
         if "_x000D_\n" in head_cell:
             lines = head_cell.split("_x000D_\n")
         elif "_x000d_\n" in head_cell:
             lines = head_cell.split("_x000d_\n")
         else:
-            assert False, "Can't split head row" + head_cell
+            raise AssertionError("Can't split head row: " + head_cell)
 
+        # validate expected Excel header string
         assert lines[0].startswith("Table"), "Unrecognized format head: " + head_cell
-        assert lines[1].endswith("The Self-Sufficiency Standard"), "Unrecognized format head: " + head_cell
+        assert lines[1].endswith("The Self-Sufficiency Standard"), (
+            "Unrecognized format head: " + head_cell
+        )
         assert lines[2] == "for", "Unrecognized format head: " + head_cell
+
         # regular expression finds the county name and the State, year
         county_match = re.search(r"^(.+)(,.?\w\w, \d\d\d\d)$", lines[3])
         assert county_match is not None, "Unrecognized format head: " + head_cell
@@ -127,15 +178,22 @@ class Snippet4ID:
         assert len(x) == 1, "should only find 1 header cell"
         # report our progress
         print("Building table for", county_name, state_etc)
-        x[0][0][1][0].text = county_name  # 1st cell - 1st paragraph - 2nd character range - Content
-        x[0][0][2][0].text = state_etc  # 1st cell - 1st paragraph - 3rd character range - Content
+        # 1st cell - 1st paragraph - 2nd character range - Content
+        x[0][0][1][0].text = county_name
+        # 1st cell - 1st paragraph - 3rd character range - Content
+        x[0][0][2][0].text = state_etc
 
-    # read the column header rows from the Excel file. The first time, we
-    # parse the cells to create keys for each column. Once we've read it once,
-    # then we just skip those rows
-    def build_column_map(self):
-        # if we've already mapped from e.g. 'aii' to column 7
+    def _build_column_map(self):
+        """
+        First time, read all column headers to create map between index & header desc.
+
+        We read up to NUM_COLUMNS_READ columns turning the column head (e.g. 'aii')
+        into the column index (e.g. 7). We only do this once. These indices are used
+        instead of the strings (i.e. using 7 instead of 'aii')
+        """
+        # if we've already created the map from e.g. 'aii' to column 7
         skip = type(self.table1_columns[0]) is int
+        # if skip is True, we still read the 7 rows the column headers use, but we skip them
         # columns will hold 250 key strings while we build them
         # start with 250 empty strings
         columns = ["" for _ in range(NUM_COLUMNS_READ)] if not skip else None
@@ -143,7 +201,7 @@ class Snippet4ID:
         for row in self.row_iterator:
             count += 1  # we read a row
             if not skip:
-                # if we need to generate
+                # if we need to generate the table
                 for i in range(min(len(columns), len(row))):
                     fam_member = row[i]  # e.g. "infant" or "teenager"
                     # if not a blank cell, add the first character in the word to the column
@@ -160,25 +218,34 @@ class Snippet4ID:
             key = columns[i]
             if len(key) > 0:
                 xl_column_map[key] = i
-        # for each requested key for table 1, replace each key (eg 'aiit') with the column index (47)
+        # for each requested key for table 1,
+        #     replace each key (eg 'aiit') with the column index (47)
         for i in range(len(self.table1_columns)):
             key = self.table1_columns[i]
             assert key in xl_column_map, "can't find table 1 column: " + key
             self.table1_columns[i] = xl_column_map[key]
 
-    # append the next table entries from excel starting at "first_row"
-    # it's split between 2 tables in InDesign
     def append_table(self, county_sheet) -> bool:
+        """
+        Append the next table from Excel starting at "first_row".
+
+        :param county_sheet: this is the Excel sheet object from openpyxl
+        :return: False if the end of the Excel was reached, True if success
+        """
         # the first time, we create the row_iterator at the top of the Excel file,
-        # but after that, the row_iterator should already be at the top of the next county data
+        # after, the row_iterator should already be at the first line of the next county
         if self.row_iterator is None:
-            self.row_iterator = county_sheet.iter_rows(min_row=1, max_row=None,
-                                                       min_col=1, max_col=NUM_COLUMNS_READ,
-                                                       values_only=True)
+            self.row_iterator = county_sheet.iter_rows(
+                min_row=1,
+                max_row=None,
+                min_col=1,
+                max_col=NUM_COLUMNS_READ,
+                values_only=True,
+            )
         try:
             # read the row into a big list
             row = next(self.row_iterator)
-        except:
+        except StopIteration:
             # if we can't read it, then we're done
             return False
 
@@ -188,12 +255,12 @@ class Snippet4ID:
         if not head_cell or len(head_cell) < 10:
             return False
 
-        # renumber the InDesign elements in the table snippets
-        self.renumber()
-        # write the correct data from the header cell to the first ID table
-        self.format_head(head_cell)
+        # prepare new table snippet by renumbering the InDesign elements
+        self._renumber()
+        # write the correct data from the header cell to the InDesign table
+        self._format_head(head_cell)
         # read or skip the column headers
-        self.build_column_map()
+        self._build_column_map()
 
         sum_tax_credits = {}
         taxes = {}
@@ -210,7 +277,7 @@ class Snippet4ID:
 
             cell = None
             if rowhead in self.tax_credits:
-                # add rows about tax credits into temporary buckets, added in later
+                # sum rows about tax credits into temporary buckets, added in later
                 sum_tax_amounts(row, self.table1_columns, sum_tax_credits)
             elif rowhead == "Taxes":
                 # add taxes into temp. bucket, too
@@ -218,12 +285,11 @@ class Snippet4ID:
             else:
                 # find the matching row in the ID table to copy the data into
                 cell = row_label_match(rowhead, self.snippet1_root)
-
-            if cell is not None:
-                copy_numbers(row, cell, self.table1_columns, rowhead == "Hourly")
+                if cell is not None:
+                    copy_numbers(row, cell, self.table1_columns, rowhead == "Hourly")
 
             if count == 20:
-                break  # if we read 20 lines, stop.
+                break  # after if we read 20 lines, stop.
 
         # we just filled out the XML tree representing the InDesign table as a snippet
         # fill out the couple of calculated entries: Taxes (Net) and Tax Credits
@@ -239,14 +305,18 @@ class Snippet4ID:
         self.snippet1_xml.write(self.id_file)
 
         # add a "force-page-break" after the table
-        self.id_file.write(b'''<CharacterStyleRange AppliedCharacterStyle="CharacterStyle/$ID/[No character style]" ParagraphBreakType="NextPage"><Br /></CharacterStyleRange>\n''')
+        self.id_file.write(
+            b"""<CharacterStyleRange AppliedCharacterStyle="CharacterStyle/$ID/"""
+            b"""[No character style]" ParagraphBreakType="NextPage">"""
+            b"""<Br /></CharacterStyleRange>\n"""
+        )
         self.id_file.close()
         return True
 
-    # finish the file by appending the "post table snippet"
     def finish(self):
+        """Finish the file by appending the post table snippet."""
         self.id_file = open(self.id_filename, "a", encoding="utf-8")
-        with open('PosttableSnippet.xml', encoding="utf-8") as postfile:
+        with open("PosttableSnippet.xml", encoding="utf-8") as postfile:
             post_data = postfile.read()
             self.id_file.write(post_data)
         self.id_file.close()
